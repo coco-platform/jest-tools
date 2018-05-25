92e472d6472b356336354161ca5e0220e725d38c
yang.yuncai <383235388@qq.com>
2018-05-25 18:03:53 +0800

    chore: remove unnecessary coverage report.

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,6 @@ module.exports = {
   },
   moduleFileExtensions: ['js', 'json'],
   moduleDirectories: ['node_modules'],
-  // Coverage report
-  collectCoverageFrom: ['lib/**/*.js'],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'html'],
   // Test configuration
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\]'],
   // Coverage report
