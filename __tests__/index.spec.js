0e8393f61e041f9ed99da6fdf02d784785515f64
yang.yuncai <383235388@qq.com>
2018-05-28 11:22:47 +0800

    feature: support static css, css modules mapping.

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -3,10 +3,57 @@
  * @author - huang.jian <hjj491229492@hotmail.com>
  */
 
-const demo = require('./__fixture__/demo.jpg');
+const path = require('path');
 
 describe('jest-tools suits', () => {
-  it('should transform status asset', () => {
+  it('should process static asset', () => {
+    // eslint-disable-next-line global-require
+    const assetTransformer = require('../lib/asset-transformer');
+    const target = path.resolve(__dirname, '__fixture__', 'demo.jpg');
+    const result = assetTransformer.process(null, target);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should transform status asset within jest', () => {
+    // eslint-disable-next-line global-require
+    const demo = require('./__fixture__/demo.jpg');
+
     expect(demo).toMatchSnapshot();
+  });
+
+  it('should ignore static css / pcss import', () => {
+    // eslint-disable-next-line global-require
+    const cssIgnore = require('../lib/css-ignore');
+
+    expect(cssIgnore).toEqual({});
+  });
+
+  it('should ignore static css / pcss import within jest', () => {
+    // eslint-disable-next-line global-require
+    const demo = require('./__fixture__/demo.css');
+
+    expect(demo).toEqual({});
+  });
+
+  it('should mapping css modules property', () => {
+    // eslint-disable-next-line global-require
+    const identityProxy = require('../lib/css-modules');
+
+    expect(identityProxy.hello).toEqual('hello');
+  });
+
+  it('should mapping css modules property', () => {
+    // eslint-disable-next-line global-require
+    const identityProxy = require('../lib/css-modules');
+
+    expect(Reflect.get(identityProxy, '__esModule')).toBeFalsy();
+  });
+
+  it('should mapping css modules property within jest', () => {
+    // eslint-disable-next-line global-require
+    const demo = require('./__fixture__/demo.pcss');
+
+    expect(demo.hello).toEqual('hello');
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,18 @@ module.exports = {
   transform: {
     '\\.(jpg|jpeg|png|giff|webp|svg)$': '<rootDir>/lib/asset-transformer.js',
   },
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/lib/css-ignore.js',
+    '\\.pcss': '<rootDir>/lib/css-modules.js',
+  },
   moduleFileExtensions: ['js', 'json'],
   moduleDirectories: ['node_modules'],
   // Test configuration
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\]'],
   // Coverage report
+  collectCoverageFrom: ['lib/*.js'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'html'],
   // Test configuration
   testEnvironment: 'node',
   testMatch: ['<rootDir>/__tests__/**/*.spec.js'],
